a398aca72af2b12d8c540bb163ea9aa988132593
hs150521 <hs150521@163.com>
2025-08-02 14:38:10 +0800

    [Feat] Untested: changed tank motors from 4SPX to 2FX, removed unused commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,9 @@
 
 package frc.robot;
 
-import frc.robot.commands.AimAtAprilTagCommand;
-import frc.robot.commands.FollowAprilTagCommand;
-import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.DriveSubsystem;
 
 
 /**
@@ -18,76 +16,68 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final DriveSubsystem m_driveSubsystem = new DriveSubsystem();
+    // The robot's subsystems and commands are defined here...
+    private final DriveSubsystem m_driveSubsystem = new DriveSubsystem();
 
-  // Creates the Xbox controller to drive the robot
-  CommandXboxController mainController = new CommandXboxController(0);  
+    // Creates the Xbox controller to drive the robot
+    CommandXboxController mainController = new CommandXboxController(0);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-  }
-
-  /** Use this method to define your trigger->command mappings. */
-  private void configureBindings() {
-    // Put any trigger->command mappings here.
-    
-    // Run motor with setSpeeds command
-    Command arcadeDrive =
-      m_driveSubsystem.run(
-        () -> {
-          m_driveSubsystem.setArcadeSpeed(
-          deadBand(-mainController.getLeftY(), 0.1),
-          deadBand(mainController.getRightX(), 0.1)
-          );
-        }
-      );
-    
-    Command tankDrive =
-      m_driveSubsystem.run(
-        () -> {
-          m_driveSubsystem.setSpeeds(
-          deadBand(-mainController.getLeftY(), 0.1),
-          deadBand(-mainController.getRightY(), 0.1)
-          );
-        }
-      );
-    
-    Command quickTurn =  
-      m_driveSubsystem.run(
-        () -> {
-          m_driveSubsystem.setSpeeds(
-          0.5,
-          -0.5
-          );
-        }
-      ).withTimeout(1.5);
-
-    Command aimAtTag = new AimAtAprilTagCommand(m_driveSubsystem);
-    Command followTag = new FollowAprilTagCommand(m_driveSubsystem);
-    
-    mainController.leftBumper().whileTrue(aimAtTag);
-    mainController.rightBumper().whileTrue(followTag);
-
-    m_driveSubsystem.setDefaultCommand(arcadeDrive);
-    
-    mainController.a().toggleOnTrue(tankDrive);
-
-  }
-
-  // Deadband command to eliminate drifting
-  public static double deadBand(double value, double tolerance) {
-    if(value < tolerance && value > -tolerance) {
-      return 0;
-    } else {
-      return value;
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        // Configure the trigger bindings
+        configureBindings();
     }
-  }
-  
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return new Command() {};
-  }
+
+    // Deadband command to eliminate drifting
+    public static double deadBand(double value, double tolerance) {
+        return Math.abs(value) > tolerance ? value : 0;
+        /*
+        If you can't understand the code above, it is equivalent to the following lines:
+        if (value < tolerance && value > -tolerance) {
+            return 0;
+        } else {
+            return value;
+        }
+        */
+    }
+
+    /**
+     * Use this method to define your trigger->command mappings.
+     */
+    private void configureBindings() {
+        // Put any trigger->command mappings here.
+
+        // Run motor with setSpeeds command
+        Command arcadeDrive =
+                m_driveSubsystem.run(
+                        () -> {
+                            m_driveSubsystem.setArcadeSpeed(
+                                    deadBand(-mainController.getLeftY(), 0.1),
+                                    deadBand(mainController.getRightX(), 0.1)
+                            );
+                        }
+                );
+
+        Command tankDrive =
+                m_driveSubsystem.run(
+                        () -> {
+                            m_driveSubsystem.setSpeeds(
+                                    deadBand(-mainController.getLeftY(), 0.1),
+                                    deadBand(-mainController.getRightY(), 0.1)
+                            );
+                        }
+                );
+
+        m_driveSubsystem.setDefaultCommand(arcadeDrive);
+
+        mainController.a().toggleOnTrue(tankDrive);
+    }
+
+    public Command getAutonomousCommand() {
+        // An example command will be run in autonomous
+        return new Command() {
+        };
+    }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,51 +4,53 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
-  /** Creates new VictorSPX motor */
-  WPI_VictorSPX motorFrontRight = new WPI_VictorSPX(1);
-  WPI_VictorSPX motorFrontLeft = new WPI_VictorSPX(0);
-  WPI_VictorSPX motorBackRight = new WPI_VictorSPX(3);
-  WPI_VictorSPX motorBackLeft = new WPI_VictorSPX(2);
+    /**
+     * Creates motors
+     */
+    TalonFX motorRight = new TalonFX(1, "rio");
+    TalonFX motorLeft = new TalonFX(0, "rio");
 
-  
-  
-  /** Creates a new DriveSubsystem. */
-  public DriveSubsystem() {
-    //Set followers
-    motorBackLeft.follow(motorFrontLeft);
-    motorBackRight.follow(motorFrontRight);
+    TalonFXConfigurator motorRightConfigurator = motorRight.getConfigurator();
+    TalonFXConfigurator motorLeftConfigurator = motorLeft.getConfigurator();
 
-    //Set Invert
-    motorFrontLeft.setInverted(false);
-    motorFrontRight.setInverted(false);
+    /**
+     * Creates a new DriveSubsystem.
+     */
+    public DriveSubsystem() {
+        MotorOutputConfigs motorOutputConfigs = new MotorOutputConfigs();
+        motorOutputConfigs.NeutralMode = NeutralModeValue.Brake;
+        motorOutputConfigs.Inverted = InvertedValue.Clockwise_Positive;
 
-    //Set Brake mode
-    motorFrontLeft.setNeutralMode(NeutralMode.Brake);
-    motorFrontRight.setNeutralMode(NeutralMode.Brake);
-  }
+        motorLeftConfigurator.apply(motorOutputConfigs);
+        motorRightConfigurator.apply(motorOutputConfigs);
+    }
 
-  /** Run motor according to joystick input values. */
-  public void setSpeeds(double leftSpeeds, double rightSpeeds) {
-    motorFrontLeft.set(ControlMode.PercentOutput, leftSpeeds);
-    motorFrontRight.set(ControlMode.PercentOutput, -rightSpeeds);
-  }
+    /**
+     * Run motor according to joystick input values.
+     */
+    public void setSpeeds(double leftSpeeds, double rightSpeeds) {
+        motorLeft.set(leftSpeeds);
+        motorRight.set(-rightSpeeds);
+    }
 
-  // Run arcade drive based on setSpeeds
-  public void setArcadeSpeed(double forwardSpeed, double turningSpeed) {
-    double leftSpeed = forwardSpeed + turningSpeed;
-    double rightSpeed = forwardSpeed - turningSpeed;
+    // Run arcade drive based on setSpeeds
+    public void setArcadeSpeed(double forwardSpeed, double turningSpeed) {
+        double leftSpeed = forwardSpeed + turningSpeed;
+        double rightSpeed = forwardSpeed - turningSpeed;
 
-    setSpeeds(leftSpeed, rightSpeed);
-  }
+        setSpeeds(leftSpeed, rightSpeed);
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
 }
