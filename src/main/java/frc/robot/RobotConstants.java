efcd11669ded25a06167650a4a9f4c615ffc3e77
....... <.......>
2025-08-06 15:06:51 +0800

    [Feat] speed based tank control

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -1,9 +1,18 @@
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
 import frc.robot.utils.TunableNumber;
 
 public class RobotConstants {
     public static final boolean TUNING = true;
+
+    public static class TankConstants {
+        public static double MAX_SPEED_METERS_PER_SECOND = 3.5; //m/s
+        public static double MAX_ANGULAR_SPEED_RAD_PER_SECOND = Math.PI; //rad/s
+        public static double GEAR_RATIO = 12.0;
+        public static double WHEEL_TRACK = 0.548005; //M
+        public static double WHEEL_RADIUS = Units.inchesToMeters(3);
+    }
 
     public static class TankPID {
         public static final TunableNumber kP = new TunableNumber("Tank_PID/KP", 0.1);
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,14 +5,16 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.configs.SlotConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.RobotConstants;
+
+import static frc.robot.RobotConstants.TankPID;
 
 public class DriveSubsystem extends SubsystemBase {
     /**
@@ -45,30 +47,39 @@ public class DriveSubsystem extends SubsystemBase {
     }
 
     public void setRPS(double leftRPS, double rightRPS) {
+        SmartDashboard.putNumber("DriveSubSystem/leftRPS", leftRPS);
+        SmartDashboard.putNumber("DriveSubSystem/rightRPS", -rightRPS);
+
         motorLeft.setControl(new VelocityVoltage(leftRPS));
         motorRight.setControl(new VelocityVoltage(-rightRPS));
-        System.out.println(leftRPS);
     }
 
-    // Run arcade drive based on setSpeeds
+    /**
+     * @param forwardPercentage: m/s
+     * @param turningDifference: rad/s
+     */
+
     public void setArcadeSpeed(double forwardPercentage, double turningDifference) {
         double leftDutycycle = forwardPercentage - turningDifference;
         double rightDutycycle = forwardPercentage + turningDifference;
-        System.out.println("leftDutycycle: " + leftDutycycle + "\n rightDutycycle" + rightDutycycle +
-                "\n forwardPercentage" + forwardPercentage + "\n turningDifference" + turningDifference);
+
+        SmartDashboard.putNumber("DriveSubSystem/leftRPS", forwardPercentage);
+        SmartDashboard.putNumber("DriveSubSystem/rightRPS", turningDifference);
+
         setRPS(leftDutycycle * 4, rightDutycycle * 4);
     }
 
     @Override
     public void periodic() {
+        motorLeft.getConfigurator().apply(new Slot0Configs()
+                .withKP(TankPID.kP.get())
+                .withKI(TankPID.kI.get())
+                .withKD(TankPID.kD.get()));
+
+        motorRight.getConfigurator().apply(new Slot0Configs()
+                .withKP(TankPID.kP.get())
+                .withKI(TankPID.kI.get())
+                .withKD(TankPID.kD.get()));
         // This method will be called once per scheduler run
-        motorLeft.getConfigurator().apply(new SlotConfigs()
-                .withKP(RobotConstants.TankPID.kP.get())
-                .withKI(RobotConstants.TankPID.kI.get())
-                .withKD(RobotConstants.TankPID.kD.get()));
-        motorRight.getConfigurator().apply(new SlotConfigs()
-                .withKP(RobotConstants.TankPID.kP.get())
-                .withKI(RobotConstants.TankPID.kI.get())
-                .withKD(RobotConstants.TankPID.kD.get()));
     }
 }
